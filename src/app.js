ed5d156854eadf930284170a05a4b7bd6f39abbd
annahico <annahico@gmail.com>
2024-05-17 21:29:29 +0200

    feat: crud authors incomplet

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 // requestAnimationFrame('dotenv').config() //asi usamos en una linia las dos siguientes
-const dotenv = require ('dotenv')
+const dotenv = require('dotenv')
 dotenv.config();
 
 const app = express();
@@ -21,3 +21,55 @@ app.listen(PORT, () => {
 })
 
 //NO SE TOCA EL CÓDIGO, PERMANECERÁ INTACTO
+
+//CRUD DE AUTHORS
+
+app.post('/api/authors', (req, res) => {
+    console.log(req.body);
+    const name = req.body.name;
+    const nationality = req.body.nationality;
+    console.log(name);
+    console.log(nationality);
+
+    app.get(
+        '/api/authors',
+        (req, res) => {
+            res.status(200).json(
+                {
+                    success: true,
+                    message: "text"
+                }
+            )
+        }
+    )
+
+    app.put(
+        'api/authors',
+        (req, res) => {
+            console.log(req.params.id);
+            res.status(200).json(
+                {
+                    success: true,
+                    message: "text"
+                }
+            )
+        })
+
+    //Validar info
+
+    if (!name) { //ejemplo
+        return res.status(400).json({
+            sucess: false,
+            message: 'Name Required'
+        })
+    }
+
+    //Si hace falta tratamos la info
+
+    let newNationality = nationality.toLowerCase()
+    console.log(newNationality);
+
+    // Atacar a la BD para crear 
+
+}
+)
