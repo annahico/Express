a2f733022a6d8e69bc77cd74694f3d878ea41d26
annahico <annahico@gmail.com>
2024-05-20 11:29:36 +0200

    feat:fixed migration loans

--- src/database/migrations/20240520092635-create-loans.js
+++ src/database/migrations/20240520092635-create-loans.js
@@ -1,28 +1,50 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Loans', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
+	async up(queryInterface, Sequelize) {
+		await queryInterface.createTable("Loans", {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER,
+			},
+			loan_date: {
+				type: Sequelize.DATE,
+				allowNull: false,
+			},
+			due_date: {
+				type: Sequelize.DATE,
+				allowNull: false,
+			},
+			return_date: {
+				type: Sequelize.DATE,
+			},
+      id_user: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id'
+        }
       },
-      name: {
-        type: Sequelize.STRING
+      id_book: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'books',
+          key: 'id'
+        }
       },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Loans');
-  }
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+		});
+	},
+	async down(queryInterface, Sequelize) {
+		await queryInterface.dropTable("Loans");
+	},
 };
