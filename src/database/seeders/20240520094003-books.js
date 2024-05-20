e32f6dc6972fd29976e6535c1efdfb9df4864c34
annahico <annahico@gmail.com>
2024-05-20 11:45:43 +0200

    feat: fixed seed migration books

--- src/database/seeders/20240520094003-books.js
+++ src/database/seeders/20240520094003-books.js
@@ -2,24 +2,37 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+
+
+    await queryInterface.bulkInsert('Books',
+      [
+        {
+          title: 'Harry Potter y la piedra filosofal',
+          gender: 'Fastasía',
+          id_author: 1,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          title: 'El Señor de los Anillos',
+          gender: 'Fastasía',
+          id_author: 2,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+      ],
+      {}
+    );
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     /**
      * Add commands to revert seed here.
      *
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
+    await queryInterface.bulkDelete('Books', null, {});
   }
 };
